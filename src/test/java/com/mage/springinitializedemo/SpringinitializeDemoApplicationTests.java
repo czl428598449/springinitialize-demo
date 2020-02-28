bf4ac70c54e0d01b59cd8e66a623d031fb9f3562
czl <428598449@qq.com>
2020-02-28 09:47:17 +0800

    Initial commit

---
+++ src/test/java/com/mage/springinitializedemo/SpringinitializeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mage.springinitializedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringinitializeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
