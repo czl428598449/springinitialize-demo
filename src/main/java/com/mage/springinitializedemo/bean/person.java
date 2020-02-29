d1cdfe54f29b89e228df98bda1cbc95175bff117
czl <428598449@qq.com>
2020-02-29 22:10:04 +0800

    springboot-demo

---
+++ src/main/java/com/mage/springinitializedemo/bean/person.java
@@ -0,0 +1,64 @@
+package com.mage.springinitializedemo.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author：czl
+ * @date:2020/2/29 19:17
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
+public class person {
+
+    private String name;
+    private int age;
+    private Map<Integer ,String> map;
+    private List<String> list;
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public Map<Integer, String> getMap() {
+        return map;
+    }
+
+
+    public void setMap(Map<Integer, String> map) {
+        this.map = map;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", map=" + map +
+                ", list=" + list +
+                '}';
+    }
+}
--- src/test/java/com/mage/springinitializedemo/SpringinitializeDemoApplicationTests.java
+++ src/test/java/com/mage/springinitializedemo/SpringinitializeDemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.mage.springinitializedemo;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringinitializeDemoApplicationTests {
+public class SpringinitializeDemoApplicationTests {
+    @Autowired
+    private com.mage.springinitializedemo.bean.person person;
 
-    @Test
-    void contextLoads() {
+  @Test
+   public void contextLoads() {
+        System.out.println(person.toString());
     }
 
 }
